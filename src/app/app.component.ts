cf73fb39e2539fc9ecf15c4993a12cf0a8d2c02c
Unknown <eddyroy2125@gmail.com>
2017-04-06 15:34:31 +0200

    Update html/css  Create angular animations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,10 @@ import { 	Component,
 			state,
 		    style,
 		    transition,
-		    animate
+			keyframes,
+		    animate,
+			ElementRef,
+			OnInit
  } from '@angular/core';
 
 export class Hero {
@@ -22,22 +25,76 @@ export class Title {
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
+  animations : [
+	trigger('ChangeColor', [
+		state('inactive',style ({
+			backgroundColor: '#586176',
+			Color: '#2a395b'
+		})),
+		state('active', style ({
+			backgroundColor: '#2a395b',
+			Color: '#d0d6e2'
+		})),
+		state('third', style ({
+			backgroundColor: 'green'
+		})),
+		
+		transition ('inactive=>active', [animate('2s ease')]),
+		transition ('active=>inactive', [animate('2s ease')]),
+		transition ('active=>third', [animate('2s ease')]),
+		transition ('third=>inactive', [animate('2s ease')])
+	]),
+	trigger('nameTranslate', [
+		transition ('void=> *',[
+			style({ transform:'translateX(-100%)'}),
+			animate('5s ease')
+			]),
+		transition ('*=> void',[
+			style({ transform:'translateX(100%)'}),
+			animate('5s ease')
+			])
+	]),
+	trigger('webTranslate', [
+		transition ('void=> *',[
+			style({ transform:'translateX(100%)'}),
+			animate('5s ease')
+			]),
+		transition ('*=> void',[
+			style({ transform:'translateX(-100%)'}),
+			animate('5s ease')
+			])
+	]),
+	trigger('navTranslate', [
+		transition ('void=> *',[
+			style({ transform:'translateY(-100%)'}),
+			animate('5s ease')
+			]),
+		transition ('*=> void',[
+			style({ transform:'translateY(100%)'}),
+			animate('5s ease')
+			])
+	])
+	]
 })
 
-export class AppComponent {
+export class AppComponent{
 	
-	title2 = "Webdeveloper";
-	section1 = "About me";
-	section2 = "Education";
-	section3 = "Skills";
-	section4 = "Portfolio";
-	section5 = "Contact";
-  name = 'Eddy';
-  age = '28';
-  hero: Hero = {
-  id: 1,
-  name: 'Hello'
-  };
+	  title2 = "Webdeveloper";
+	  section1 = "About me";
+	  sectionState: string = 'inactive';
+	  changeColor(){
+		  this.sectionState= (this.sectionState === 'inactive') ? 'active' : 'inactive' && (this.sectionState === 'active') ? 'third' : 'active' && (this.sectionState === 'third') ? 'inactive' : 'third';
+	  }
+	  section2 = "Education";
+	  section3 = "Skills";
+	  section4 = "Portfolio";
+	  section5 = "Contact";
+	  name = 'Eddy';
+	  age = '28';
+	  hero: Hero = {
+	  id: 1,
+	  name: 'Hello'
+	  };
 
 	title : Title = {	
 	name: 'Eddy ROY,'
